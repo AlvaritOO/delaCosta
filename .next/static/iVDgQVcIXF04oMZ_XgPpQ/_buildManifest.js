1800bc72a03f648cd8a9064072cd3e46d19c8472
Álvaro Medina <alvaro.e.medina.a@gmail.com>
2023-05-25 12:03:50 -0400

    ++ img

---
+++ .next/static/iVDgQVcIXF04oMZ_XgPpQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002Fe9feda9bb25c24811b10.css","static\u002Fchunks\u002Fpages\u002Findex-ef5e9fb7269dd669970a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
