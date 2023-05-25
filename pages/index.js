86821f566537a7a255dc1e78d1afda5ea28a1fb4
Álvaro Medina <alvaro.e.medina.a@gmail.com>
2023-05-25 11:58:20 -0400

    ++ imagenes

--- pages/index.js
+++ pages/index.js
@@ -1,111 +1,191 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import RancaguaBBQ from "../pages/src/img/RancaguaBBQ.jpg";
-import Americana from "../pages/src/img/Americana.jpg";
-import CuatroCarnes from "../pages/src/img/CuatroCarnes.jpg";
-import Hawaiana from "../pages/src/img/Hawaiana.jpg";
-import CuatroQueso from "../pages/src/img/CuatroQueso.jpg";
-import Italiana from "../pages/src/img/Italiana.jpg";
-import Napolitana from "../pages/src/img/Napolitana.jpg";
-import SuperPepperoni from "../pages/src/img/SuperPepperoni.jpg";
-import Vegetariana from "../pages/src/img/Vegetariana.jpg";
+import LogoDelacosta from "../pages/src/img/logodelacosta.jpeg";
+
+import AtunCarpacho from "../pages/src/img/atuncarpacho.jpeg";
+import AtunCocido from "../pages/src/img/atuncocido.jpeg";
+import AtunSashimi from "../pages/src/img/atunsashimi.jpeg";
+import Camarones from "../pages/src/img/camarones_.jpeg";
+import CamaronesAlPilpil from "../pages/src/img/camaronesalpilpil.jpeg";
+import ChupeDeLocos from "../pages/src/img/chupedelocos.jpeg";
+import Congrio from "../pages/src/img/congrio.jpeg";
+import Eriso from "../pages/src/img/eriso.jpeg";
+import ErisoEnsalada from "../pages/src/img/erisoensalada.jpeg";
+import JaivaEnsalada from "../pages/src/img/jaivaensalada.jpeg";
+import Locos from "../pages/src/img/locos.jpeg";
+import Machas from "../pages/src/img/machas.jpeg";
+import Ostion from "../pages/src/img/ostion.jpeg";
+import OstionesAlaParmesana from "../pages/src/img/ostionesalaparmesana.jpeg";
+import PinsasDeJaiva from "../pages/src/img/pinsasdejaiva.jpeg";
+import Piure from "../pages/src/img/piure.jpeg";
+import Pulpo from "../pages/src/img/pulpo.jpeg";
+import Reineta from "../pages/src/img/reineta.jpeg";
+import ReinetaAlaPlancha from "../pages/src/img/reinetaalaplancha.jpeg";
+import Salmon from "../pages/src/img/salmon.jpeg";
+import SalmonAlaPlancha from "../pages/src/img/salmonalaplancha.jpeg";
+import SalmonCrudo from "../pages/src/img/salmoncrudo.jpeg";
+
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>FullPizzas - Pizzería a domicilio en Rancagua</title>
+        <title>De la Costa - Podructos del mar a domicilio en Rancagua y Machali</title>
       </Head>
 
       <main className={styles.main}>
+      <img src={LogoDelacosta} alt="LogoDelacosta" className={styles.img} />
         <h1 className={styles.title}>
-          Bienvenidos a{" "}
-          <a href="https://www.ubereats.com/rancagua/food-delivery/fullpizzas/5CmH35u_RaWJ0ZEidaBRfg?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMlJhbmNhZ3VhJTIyJTJDJTIycmVmZXJlbmNlJTIyJTNBJTIyQ2hJSjB4UHZ2V2REWTVZUjkxU3Rab1A4YzlrJTIyJTJDJTIycmVmZXJlbmNlVHlwZSUyMiUzQSUyMmdvb2dsZV9wbGFjZXMlMjIlMkMlMjJsYXRpdHVkZSUyMiUzQS0zNC4xNzAxMzI0MDAwMDAwMSUyQyUyMmxvbmdpdHVkZSUyMiUzQS03MC43NDA2MjU5JTdE">
-            FullPizzas!
-          </a>
+          Bienvenidos
         </h1>
-        <p className={styles.description}>La casa de la Super Goloza</p>{" "}
-        <div className={styles.button}>
-          <a href="https://wa.me/message/CTYZQI5JZWJTO1">
-            <h3>Envia un WhatsApp &rarr;</h3>
-          </a>
-        </div>
+        <p className={styles.description}>Podructos del mar a domicilio</p>{" "}
+        
         <div className={styles.grid}>
+
           <a href="" className={styles.card}>
-            <img src={RancaguaBBQ} alt="RancaguaBBQ" className={styles.img} />
-            <h3>Rancagua BBQ</h3>
-            <p>Queso, orégano, pepperoni, pollo, tocino y salsa barbacoa.</p>
+            <img src={AtunCarpacho} alt="AtunCarpacho" className={styles.img} />
+            <h3>Atún carpacho</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Americana} alt="Americana" className={styles.img} />
-            <h3>Americana</h3>
-            <p>Queso, orégano, tocino, pollo, salame y choricillo.</p>
+            <img src={AtunCocido} alt="AtunCocido" className={styles.img} />
+            <h3>Atún Cocido</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={CuatroCarnes} alt="CuatroCarnes" className={styles.img} />
-            <h3>Cuatro Carnes</h3>
-            <p>Queso, orégano, pepperoni, lomo, salame y choricillo</p>
+            <img src={AtunSashimi} alt="AtunCocido" className={styles.img} />
+            <h3>Atún Sashimi</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Hawaiana} alt="Hawaiana" className={styles.img} />
-            <h3>Hawaiana</h3>
-            <p>Extra queso mozzarella, gauda, orégano, piña y jamón.</p>
+            <img src={Camarones} alt="AtunCocido" className={styles.img} />
+            <h3>Camarones</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={CuatroQueso} alt="CuatroQueso" className={styles.img} />
-            <h3>Cuatro Quesos</h3>
-            <p>
-              Salsa de tomate, orégano, queso mozzarella, gauda, edam y queso
-              azul.
-            </p>
+            <img src={CamaronesAlPilpil} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Camarones al pil pil</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Italiana} alt="Italiana" className={styles.img} />
-            <h3>Italiana</h3>
-            <p>Queso, orégano, aceitunas negras, champiñón y salame.</p>
+            <img src={ChupeDeLocos} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Chupe de locos</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Napolitana} alt="Napolitana" className={styles.img} />
-            <h3>Napolitana</h3>
-            <p>Extra queso mozzarella, gauda, orégano, tomate y jamón.</p>
+            <img src={Congrio} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Congrio</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img
-              src={SuperPepperoni}
-              alt="SuperPepperoni"
-              className={styles.img}
-            />
-            <h3>Super Pepperoni</h3>
-            <p>Queso mozzarella, gauda, orégano y doble pepperoni.</p>
+            <img src={Eriso} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Eriso</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Vegetariana} alt="Vegetariana" className={styles.img} />
-            <h3>Vegetariana</h3>
-            <p>
-              Queso, orégano, pimiento verde, tomate, aceitunas negras y
-              champiñón.
-            </p>
+            <img src={ErisoEnsalada} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Eriso Ensalada</h3>
+            <p>descripcion....</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={CuatroCarnes} alt="CuatroCarnes" className={styles.img} />
-            <h3>Española</h3>
-            <p>
-              Queso, orégano, pimiento verde, tomate, lomito de cerdo y
-              choricillo
-            </p>
+            <img src={JaivaEnsalada} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Jaiva ensalada</h3>
+            <p>descripcion....</p>
           </a>
-          <a className={styles.card}>
-            Beta Version - Sitio web en construccion - FullPizzas - Rancagua
+
+          <a href="" className={styles.card}>
+            <img src={Locos} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Locos</h3>
+            <p>descripcion....</p>
           </a>
+
+          <a href="" className={styles.card}>
+            <img src={Machas} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Machas</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={Ostion} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Ostion</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={OstionesAlaParmesana} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Ostiones a la parmesana</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={PinsasDeJaiva} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Pinsas de jaiva</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={Piure} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Piure</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={Pulpo} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Pulpo</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={Reineta} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Reineta</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={ReinetaAlaPlancha} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>ReinetaAlaPlancha</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={Salmon} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Salmon</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={SalmonAlaPlancha} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Salmon a la plancha</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <a href="" className={styles.card}>
+            <img src={SalmonCrudo} alt="CamaronesAlPilpil" className={styles.img} />
+            <h3>Salmon crudo</h3>
+            <p>descripcion....</p>
+          </a>
+
+          <br></br>
+
+          <div className={styles.button}>
+            <a href="https://wa.me/message/CTYZQI5JZWJTO1">
+              <p>Envia un WhatsApp &rarr;</p>
+            </a>
+          </div>
+
+          <h2>
+            Machali - Rancagua
+          </h2>
         </div>
       </main>
       <footer className={styles.footer}>
